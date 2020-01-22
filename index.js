1d1f2b671eab0744dbb4307c39b5c73115c6bc09
Depasse Arthur <arthur.depasse96@gmail.com>
2020-01-22 22:27:25 +0000

    changed port to env

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.use(
     )
   })
 );
-
-app.listen(5000, function() {
-  console.log("Example app listening on port 3000!");
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Example app listening on port", port);
 });
